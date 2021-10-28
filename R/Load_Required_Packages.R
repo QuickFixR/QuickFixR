f08b3a6b52e519a750edd58a4480b433342b405c
mah-kh58 <khezrian@web.de>
2021-10-28 23:53:22 +0330

    Firt Commit.

--- R/Load_Required_Packages.R
+++ R/Load_Required_Packages.R
@@ -1,24 +1,27 @@
 #Load_Required_Packages.R
 #2021/10/28 - 16:53
-varPackages <-
-  base::data.frame(
-    pkgName = c("devtools"
-                ,"magrittr"
-                ,"available"
-    ));
 
-base::apply(varPackages
-            , 1
-            , function(x)
-            {
-              varPkgName <- x[["pkgName"]];
-              print(varPkgName);
-              if(!base::require(package = varPkgName , character.only = TRUE))
-                utils::install.packages(pkgs = varPkgName);
-              #base::library(package =varPkgName , character.only = TRUE );
-            });
+# varPackages <-
+#   base::data.frame(
+#     pkgName = c("devtools"
+#                 ,"magrittr"
+#                 ,"available"
+#     ));
+#
+# base::apply(varPackages
+#             , 1
+#             , function(x)
+#             {
+#               varPkgName <- x[["pkgName"]];
+#               print(varPkgName);
+#               if(!base::require(package = varPkgName , character.only = TRUE))
+#                 utils::install.packages(pkgs = varPkgName);
+#               #base::library(package =varPkgName , character.only = TRUE );
+#             });
 
-rm(varPackages);
+#rm(varPackages);
+
+#usethis::use_readme_rmd();
 
 
 
--- R/hello.R
+++ R/hello.R
@@ -13,12 +13,24 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
+#' Test Function For Package
+#'
+#' @param x The Name Of The Person To Say Hello.
+#'
+#' @return The output from \code{\link{print}}
+#' @export
+#'
+#' @examples
+#' hello("Mahmood Jan")
+#' \dontrun{
+#' hello("Ziba Jan")
+#' }
 hello <- function(x) {
   y <- 100;
   y <- y + 100;
   y <- y + 200;
-  
+
   #Add This Comment From Git Web.
-  
+
   print(base::paste0("Hello, [",x,"]!"));
 }
