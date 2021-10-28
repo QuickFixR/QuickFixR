425144aa913403542df42c66b5729c43b2bc6c21
mah-kh58 <khezrian@web.de>
2021-10-28 18:31:27 +0330

    Publish Project To GitHub.

---
+++ R/Load_Required_Packages.R
@@ -0,0 +1,21 @@
+#Load_Required_Packages.R
+#2021/10/28 - 16:53
+varPackages <-
+  base::data.frame(
+    pkgName = c("devtools"
+                ,"magrittr"
+                ,"available"
+    ));
+
+base::apply(varPackages
+            , 1
+            , function(x)
+            {
+              varPkgName <- x[["pkgName"]];
+              print(varPkgName);
+              if(!base::require(package = varPkgName , character.only = TRUE))
+                utils::install.packages(pkgs = varPkgName);
+              #base::library(package =varPkgName , character.only = TRUE );
+            });
+
+rm(varPackages);
